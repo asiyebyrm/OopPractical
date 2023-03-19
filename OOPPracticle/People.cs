2c67d76671210fb468a8d3b1e9e15183fd46f40b
Asiye <asiyebayram1190@gmail.com>
2023-03-19 13:13:46 +0300

    Create object student and people.

---
+++ OOPPracticle/People.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPPracticle
+{
+    internal class People
+    {
+        private string name;
+        private int age;
+        private string birthday;
+        private string job;
+    }
+}
